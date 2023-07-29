d70ec439a5fdcf6b4f66e9a60447f4a188f7ce7d
Caio lima <caiohenrys50@gmail.com>
2023-07-29 02:24:01 -0300

    style card

---
+++ src/components/MyCards/styles.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+
+export const Container = styled.div`
+    width: 100%;
+    text-align: center;
+
+`
+
+export const CardsContainer = styled.div`
+  width: 100%;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  max-width: 350px;
+  height: 220px;
+  border-radius: 16px;
+  margin: 40px 45px auto;
+  margin-bottom: 20px;
+  padding: 20px;
+  background: #ADA996;
+  background: -webkit-linear-gradient(
+    to right,
+    #eaeaea,
+    #dbdbdb,
+    #f2f2f2,
+    #ada996
+  );
+  background: linear-gradient(to right, #eaeaea, #dbdbdb, #f2f2f2, #ada996);
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  display: inline-block;
+  flex-direction: column;
+
+`;
