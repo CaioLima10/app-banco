295832bfdff1e162e333487519c1ab834113b3c4
Caio lima <caiohenrys50@gmail.com>
2023-07-29 01:03:21 -0300

    add sidebaritem

---
+++ src/components/SideBarItem/SideBarItem.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+import { Container } from "./style";
+
+export default function SideBarItem({ Icon , Text , to }) {
+  return (
+      <Link to={to}>
+        <Container>
+            <Icon/>
+            {Text}
+        </Container>
+      </Link>
+  )
+}
+  
