bcdf2d19c6b7530ea344f0d07920282b27ef07d9
Caio lima <caiohenrys50@gmail.com>
2023-07-29 13:59:56 -0300

    fixing mistakes

--- src/components/MyCards/MyCards.jsx
+++ src/components/MyCards/MyCards.jsx
@@ -1,87 +1,85 @@
-
 import { CardsContainer, Container } from "./styles";
-import Header from "../Header/Header"
-import { useState } from "react";
+import Header from "../Header/Header";
+import React, { useState, useEffect } from "react";
 
 export default function MyCards() {
+  const storedCards = JSON.parse(localStorage.getItem('cards')) || [];
+  const filteredCards = storedCards.filter((card) => card !== null);
 
-  const [deleteCard, setDeleteCard] = useState(JSON.parse(localStorage.getItem('cards')) || []);
-  const [createCard, setCreateCards] = useState(JSON.parse(localStorage.getItem('cards')) || []);
+  const [deleteCard, setDeleteCard] = useState(filteredCards);
+  const [createCards, setCreateCards] = useState(filteredCards);
 
-  console.log(createCard)
+  useEffect(() => {
+    localStorage.setItem('cards', JSON.stringify(createCards));
+  }, [createCards]);
+
+  const addSpacesToCardNumber = (number) => {
+    return number.replace(/\s/g, '').replace(/(\d{4})/g, '$1 ').trim();
+  };
+
+  const addSpacesToCardExpiry = (expiry) => {
+    return expiry.replace(/(\d{2})(\d{2})/, '$1/$2').trim();
+  };
 
 
-    const createCards = JSON.parse(localStorage.getItem('cards')) || [];
-  
-    const addSpacesToCardNumber = (number) => {
-      return number.replace(/\s/g, '').replace(/(\d{4})/g, '$1 ').trim();
-    };
-  
-      const addSpacesToCardExpiry = (expiry) => {
-        return expiry.replace(/(\d{2})(\d{2})/, '$1/$2').trim(); 
+  const handleClickBlock = (id) => {
+    const updatedCards = [...createCards];
+
+    const updatedCardsWithBlock = updatedCards.map((card) => {
+      if (card.id === id) {
+        return {
+          ...card,
+          isCompleted: !card.isCompleted,
+        };
       }
+      return card;
+    });
 
-      const handleDeleteCard = (id) => {
-    
+    setCreateCards(updatedCardsWithBlock);
+  };
 
+  const handleDeleteCard = (id) => {
     const cardIndex = deleteCard.findIndex((card) => card.id === id);
-        
-        if (cardIndex !== -1) {
-  
-        const updateCard = [...deleteCard];      
-        updateCard.splice(cardIndex, 1);
-        setDeleteCard(updateCard);
-        localStorage.setItem('cards', JSON.stringify(updateCard));
-        }    
-      };
 
-      const handleClickBlock = (id) => {
-        const updatedCards = JSON.parse(localStorage.getItem('cards')) || [];
+    if (cardIndex !== -1) {
+      const updateCard = [...deleteCard];
+      updateCard.splice(cardIndex , 1);
       
-        const updatedCardsWithBlock = updatedCards.map((card) => {
-          if (card.id === id) {
-            return {
-              ...card,
-              isCompleted: !card.isCompleted,
-            };
-          }
-          return card;
-        });
+      setDeleteCard(updateCard);
       
-        setCreateCards(updatedCardsWithBlock);
-        localStorage.setItem('cards', JSON.stringify(updatedCardsWithBlock));
-      };
-      
-      
-  
+      setCreateCards(updateCard)
+
+      localStorage.setItem('cards' , JSON.stringify(updateCard))
+    }
+  };
 
   return (
-  <>
-      <Header/>
-    <Container>
-    {createCards.map((item ) => (
-      <>   
-      <CardsContainer key={item.id}
-        $completed={item.isCompleted ? 'checked-block' : 'remove-block'}
-        className={item.isCompleted ? "checked-block" : ""}
-        disabled={item.isCompleted}
-      >
-        <div>
-          <h2>{addSpacesToCardNumber(item.number)}</h2>
-          <h3>{item.name}</h3>
-          <span>{addSpacesToCardExpiry(item.expiry)}</span>
-          <p>{item.cvc}</p>
-        </div>
-        <div>
-          <button onClick={() => handleDeleteCard(item.id)} >confirmar</button>
-        </div>
-      </CardsContainer>
-         <div>
-            <button className="btn-block" onClick={ () => handleClickBlock(item.id) }>bloquear</button>
-        </div>
-      </>
-      ))}
-    </Container>
-  </>
-  )
+    <>
+      <Header />
+      <Container>
+        {createCards.map((item , index) => (
+          <React.Fragment key={index}>
+            <CardsContainer
+              $completed={item.isCompleted ? 'checked-block' : 'remove-block'}
+              className={item.isCompleted ? "checked-block" : ""}
+              disabled={item.isCompleted}
+            >
+              <div>
+                <h2>{addSpacesToCardNumber(item.number)}</h2>
+                <h3>{item.name}</h3>
+                <span>{addSpacesToCardExpiry(item.expiry)}</span>
+                <p>{item.cvc}</p>
+              </div>
+              <div>
+                <button onClick={() => handleDeleteCard(item.id)}>confirmar</button>
+              </div>
+            </CardsContainer>
+            <div>
+              <button className="btn-block" onClick={() => handleClickBlock(item.id)}>bloquear</button>
+            </div>
+          </React.Fragment>
+        ))}
+      </Container>
+    </>
+  );
 }
