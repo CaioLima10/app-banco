369e140f3cb629e54f5a3bae33d2a90a25d2ac1a
Caio lima <caiohenrys50@gmail.com>
2023-07-29 02:23:34 -0300

    boder

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -2,11 +2,13 @@ import { styled } from "styled-components"
 import {FaBars} from "react-icons/fa"
 
 export const Container = styled.header`
-    height: 70px;
+    height: 150px;
     display: flex;
     background: #000000;  
     background: -webkit-linear-gradient(to right, #e74c3c, #000000); 
     background: linear-gradient(to right, #e74c3c, #000000); 
+    border-bottom-left-radius: 100px;
+    border-bottom-right-radius: 100px;
 
 `
 
