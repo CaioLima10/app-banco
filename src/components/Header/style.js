a25e812e96f392d815d8f4ee2928046f3937a5bd
Caio lima <caiohenrys50@gmail.com>
2023-07-30 02:39:30 -0300

    refactoring icon size

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -2,21 +2,19 @@ import { styled } from "styled-components"
 import {FaBars} from "react-icons/fa"
 
 export const Container = styled.header`
-    height: 150px;
+    height: 60px;
     display: flex;
     background: #000000;  
     background: -webkit-linear-gradient(to right, #e74c3c, #000000); 
     background: linear-gradient(to right, #e74c3c, #000000); 
-    border-bottom-left-radius: 100px;
-    border-bottom-right-radius: 100px;
 
 `
 
 export const Hamburguer = styled(FaBars)`
     color: #ededed;
-    width: 30px;
+    width: 20px;
     height: 30px;
-    margin-top: 18px;
+    margin-top: 14px;
     margin-left: 32px;
     cursor: pointer;
 `
