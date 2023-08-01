a065b7919dd3ca6106f40792d28c54a23d64c932
Caio lima <caiohenrys50@gmail.com>
2023-08-01 09:16:19 -0300

    add new route

--- src/components/routes/index.jsx
+++ src/components/routes/index.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter , Route , Routes } from "react-router-dom";
 import CreditCard from "../Card/Card";
 import MyCards from "../MyCards/MyCards";
+import Pix from "../Pix/Pix";
 
 export default function RouterApp () {
   return (
@@ -9,6 +10,8 @@ export default function RouterApp () {
         <Routes>
             <Route exact path="/" element={<CreditCard/>} />
             <Route  path="/meus-cartoes" element={<MyCards/>} />
+            <Route  path="/pagamentos" element={<Pix/>} />
+
             <Route  path="*" element={<CreditCard/>} />
         </Routes>
     </BrowserRouter>
