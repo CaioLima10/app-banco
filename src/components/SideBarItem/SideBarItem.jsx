b5190957bf011ef0ee826bac89b41b14b24772de
Caio lima <caiohenrys50@gmail.com>
2023-08-07 12:00:29 -0300

    add fragmet

--- src/components/SideBarItem/SideBarItem.jsx
+++ src/components/SideBarItem/SideBarItem.jsx
@@ -3,12 +3,14 @@ import { Container } from "./style";
 
 export default function SideBarItem({ Icon , Text , to }) {
   return (
+    <>  
       <Link to={to}>
         <Container>
             <Icon/>
             {Text}
         </Container>
       </Link>
+    </>
   )
 }
   
