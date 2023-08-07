b810ac6aca1a033746180efba05042ae5010dacd
Caio lima <caiohenrys50@gmail.com>
2023-08-07 12:00:14 -0300

    fixing sidebar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,9 +1,9 @@
 import { FaHome  } from "react-icons/fa"
-import { Close, Container, Content } from "./style"
+import { Close, Container, ContainerContent, Content } from "./style"
 import SideBarItem from "../SideBarItem/SideBarItem"
 import { AiFillCreditCard } from 'react-icons/ai'
 import { MdPix } from 'react-icons/md'
-import { PiNewspaperBold } from 'react-icons/pi'
+import Logo from "../../assets/BANK-removebg-preview 1.png"
 
 export default function SideBar({ active }) {
 
@@ -14,12 +14,14 @@ export default function SideBar({ active }) {
   return (
     <Container sidebar={active}>
         <Close onClick={closeSideBar}/> 
+       <ContainerContent>       
         <Content >
-            <SideBarItem Icon={FaHome}  Text="Home" to="/" />
-            <SideBarItem Icon={AiFillCreditCard} Text="Meus Cartoes" to="meus-cartoes"/>
-            <SideBarItem Icon={MdPix} Text="Faça seu Pix" />
-            <SideBarItem Icon={PiNewspaperBold} Text="Extrato da conta "/>
+            <SideBarItem  Icon={FaHome}  Text="Home" to="/" />
+            <SideBarItem Icon={AiFillCreditCard} Text="Crie seu Cartão" to="/criar"/>
+            <SideBarItem Icon={AiFillCreditCard} Text="Meus Cartões" to="/meus-cartoes"/>
+            <SideBarItem Icon={MdPix} Text="Faça seu Pix" to="/pagamentos" />
         </Content>
+        </ContainerContent> 
     </Container>
   )
 }
