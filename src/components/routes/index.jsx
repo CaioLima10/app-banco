e08cb30eb22b1664837fd3ea9dbb385c5643a74a
Caio lima <caiohenrys50@gmail.com>
2023-08-07 11:59:36 -0300

    add routes

--- src/components/routes/index.jsx
+++ src/components/routes/index.jsx
@@ -2,17 +2,20 @@ import { BrowserRouter , Route , Routes } from "react-router-dom";
 import CreditCard from "../Card/Card";
 import MyCards from "../MyCards/MyCards";
 import Pix from "../Pix/Pix";
+import Home from "../Home/Home";
+import Sent from "../Pix/Sent";
 
 export default function RouterApp () {
   return (
     <>  
     <BrowserRouter>
         <Routes>
-            <Route exact path="/" element={<CreditCard/>} />
+            <Route exact path="/" element={<Home/>} />
+            <Route exact path="/criar" element={<CreditCard/>} />
             <Route  path="/meus-cartoes" element={<MyCards/>} />
-            <Route  path="/pagamentos" element={<Pix/>} />
-
-            <Route  path="*" element={<CreditCard/>} />
+            <Route  exact path="/pagamentos" element={<Pix/>} />
+            <Route exact path="/enviado" element={<Sent/>} />
+            <Route  path="*" element={<Home/>} />
         </Routes>
     </BrowserRouter>
     </>
