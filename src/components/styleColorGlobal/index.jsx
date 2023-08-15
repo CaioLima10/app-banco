cf0ddfe134b1d8bd8ce1eb4688beda52a494afb0
Caio lima <caiohenrys50@gmail.com>
2023-08-15 05:32:15 -0300

    img global

---
+++ src/components/styleColorGlobal/index.jsx
@@ -0,0 +1,10 @@
+import  imgContainer  from "../../assets/Group 47.png"
+import { Img } from "./style"
+
+export default function StyleColorGlobal() {
+  return (
+    <>
+        <Img src={imgContainer} alt="" />
+    </>
+    )
+}
