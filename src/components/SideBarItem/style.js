612326dbac94bd6119d66230318c95eed3527cc0
Caio lima <caiohenrys50@gmail.com>
2023-08-15 10:07:50 -0300

    add active

--- src/components/SideBarItem/style.js
+++ src/components/SideBarItem/style.js
@@ -5,7 +5,6 @@ export const Container = styled.div`
     align-items: center;
     font-size: 18px;
     color: #000;
-    background-color: #9ac2ad99;
     border-radius: 5px;
     padding: 18px;
     left: ${ props => props.sidebar ? '0' : '-100%'};
@@ -13,11 +12,14 @@ export const Container = styled.div`
     cursor: pointer;
     margin: 0 6px 8px;
     width: 328px;
-    
+
     &:hover{
+        background-color: #AED8EF99;
+    }
+    &:active{
         border: 1px solid #0375B4;
     }
-    
+
     > svg{
         margin: 0 20px;
         
