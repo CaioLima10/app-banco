6274a2e73dc50d3febc643ad2d6e44180732dce0
Caio lima <caiohenrys50@gmail.com>
2023-08-15 05:31:44 -0300

    remove route creaditCard

--- src/components/routes/index.jsx
+++ src/components/routes/index.jsx
@@ -2,20 +2,19 @@ import { BrowserRouter , Route , Routes } from "react-router-dom";
 import CreditCard from "../Card/Card";
 import MyCards from "../MyCards/MyCards";
 import Pix from "../Pix/Pix";
-import Home from "../Home/Home";
 import Sent from "../Pix/Sent";
+import { CreateCards } from "../MyCards/styles";
 
 export default function RouterApp () {
   return (
     <>  
     <BrowserRouter>
         <Routes>
-            <Route exact path="/" element={<Home/>} />
-            <Route exact path="/criar" element={<CreditCard/>} />
+            <Route exact path="/" element={<CreditCard/>} />
             <Route  path="/meus-cartoes" element={<MyCards/>} />
             <Route  exact path="/pagamentos" element={<Pix/>} />
             <Route exact path="/enviado" element={<Sent/>} />
-            <Route  path="*" element={<Home/>} />
+            <Route  path="*" element={<CreateCards/>} />
         </Routes>
     </BrowserRouter>
     </>
