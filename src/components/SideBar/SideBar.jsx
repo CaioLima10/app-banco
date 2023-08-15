b69e8c3f01a262616b985ecdd1b7ccfaacc754c7
Caio lima <caiohenrys50@gmail.com>
2023-08-15 10:07:24 -0300

    add img logo

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -3,8 +3,7 @@ import { Close, Container, ContainerContent, Content } from "./style"
 import SideBarItem from "../SideBarItem/SideBarItem"
 import { AiFillCreditCard } from 'react-icons/ai'
 import { MdPix } from 'react-icons/md'
-import Logo from "../../assets/BANK-removebg-preview 1.png"
-
+import  Logo  from "../../assets/LOGO-GOLD.png"
 export default function SideBar({ active }) {
 
     const closeSideBar = () => {
@@ -12,6 +11,7 @@ export default function SideBar({ active }) {
     }
 
   return (
+    <>
     <Container sidebar={active}>
         <Close onClick={closeSideBar}/> 
        <ContainerContent>       
@@ -19,8 +19,12 @@ export default function SideBar({ active }) {
             <SideBarItem  Icon={FaHome}  Text="Home" to="/" />
             <SideBarItem Icon={AiFillCreditCard} Text="Meus Cartões" to="/meus-cartoes"/>
             <SideBarItem Icon={MdPix} Text="Faça seu Pix" to="/pagamentos" />
+            <div>
+              <img src={Logo} alt="LOGO" />
+            </div>
         </Content>
         </ContainerContent> 
     </Container>
+    </>
   )
 }
