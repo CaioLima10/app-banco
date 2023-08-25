d2515307ccf79e28003bfaa640901dab83fb315a
Caio lima <caiohenrys50@gmail.com>
2023-08-24 23:48:58 -0300

    style scroll

---
+++ src/BackToTopButton/style.js
@@ -0,0 +1,18 @@
+import { BsFillArrowUpSquareFill } from "react-icons/bs";
+import { styled } from "styled-components";
+
+export const Button = styled.button`
+    position: fixed;
+    bottom: 50px;
+    right: 50px;
+    height: 40px;
+    width: 40px;
+    border-radius: 5px;
+    background-color: #FFFFFF;
+    border: none;
+    cursor: pointer;
+`
+
+export const ArrowUp = styled(BsFillArrowUpSquareFill)`
+    font-size: 46px;
+`
