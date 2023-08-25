b8751324d1ddb1beb2598d9b24948e2098bb428b
Caio lima <caiohenrys50@gmail.com>
2023-08-25 02:49:12 -0300

    implementing new routes

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,14 +1,20 @@
 import { FaHome  } from "react-icons/fa"
-import { Close, Container, ContainerContent, Content } from "./style"
+import {  Close, Container, ContainerContent, Content } from "./style"
 import SideBarItem from "../SideBarItem/SideBarItem"
 import { AiFillCreditCard } from 'react-icons/ai'
+import { TbSquareRoundedPlusFilled } from "react-icons/tb"
+import { PiNewspaperClippingBold } from "react-icons/pi"
 import { MdPix } from 'react-icons/md'
-import  Logo  from "../../assets/LOGO-GOLD.png"
+import  Logo  from "../../assets/BANK-logo.png"
+import Account from "../Pix/Acconut/acconut"
+
 export default function SideBar({ active }) {
 
     const closeSideBar = () => {
         active(false)
     }
+
+    
 
   return (
     <>
@@ -16,9 +22,12 @@ export default function SideBar({ active }) {
         <Close onClick={closeSideBar}/> 
        <ContainerContent>       
         <Content >
+          <Account/>
             <SideBarItem  Icon={FaHome}  Text="Home" to="/" />
+            <SideBarItem Icon={TbSquareRoundedPlusFilled} Text="Criar Cartões" to="/criar"/>
             <SideBarItem Icon={AiFillCreditCard} Text="Meus Cartões" to="/meus-cartoes"/>
             <SideBarItem Icon={MdPix} Text="Faça seu Pix" to="/pagamentos" />
+            <SideBarItem  Icon={PiNewspaperClippingBold}  Text="Extrato" to="/extrato" />
             <div>
               <img src={Logo} alt="LOGO" />
             </div>
