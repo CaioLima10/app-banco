baa3a38af75992b0483f0b3032f62372d3b8d8b6
Caio lima <caiohenrys50@gmail.com>
2023-08-25 02:45:19 -0300

    add home

---
+++ src/components/Home/home.jsx
@@ -0,0 +1,57 @@
+import Header from "../Header/Header"
+import { Logo, Section, StyledIcon, StyledLink } from "./style"
+import { MdOutlineAddCard, MdPix } from "react-icons/md";
+import { BsCreditCard2Back, 
+          BsNewspaper } from "react-icons/bs"
+
+import SigmaBank from "../../assets/BANK-removebg-preview 1.png"          
+import BackToTopButton from "../../BackToTopButton";
+
+
+export default function Home() {
+
+
+  return (
+    <>  
+    <Header/>
+    <Logo className="logo">
+        <img src={SigmaBank} alt="Logo" />
+    </Logo>
+    <Section>
+      <div className="container-card">
+        <StyledLink to={"/criar"}>    
+          <button className="content">
+                <StyledIcon as={MdOutlineAddCard} />
+                <span>Criar cartão digital</span>
+          </button>
+        </StyledLink>
+      </div>
+      <div className="container-card">
+        <StyledLink to={"/meus-cartoes"}>  
+          <button className="content">
+            <StyledIcon as={BsCreditCard2Back}/>
+            <span>Seus cartões</span>
+          </button>
+        </StyledLink>
+      </div>
+      <div className="container-card">
+        <StyledLink to={"/pagamentos"}>  
+          <button className="content">
+              <StyledIcon as={MdPix} />
+              <span>Pix</span>
+          </button>
+        </StyledLink>
+      </div>
+      <div className="container-card">
+        <StyledLink to={"/extrato"}>  
+          <button className="content-extract">
+            <StyledIcon  as={BsNewspaper}/>
+            <span>Extrato</span>
+          </button>
+        </StyledLink>  
+      </div>
+    </Section>
+      <BackToTopButton/>
+    </>
+  )
+}
