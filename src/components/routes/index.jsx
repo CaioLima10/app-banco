311903565474712becd923a5bc975516c4677473
Caio lima <caiohenrys50@gmail.com>
2023-08-25 02:48:29 -0300

    modifying routes

--- src/components/routes/index.jsx
+++ src/components/routes/index.jsx
@@ -2,19 +2,22 @@ import { BrowserRouter , Route , Routes } from "react-router-dom";
 import CreditCard from "../Card/Card";
 import MyCards from "../MyCards/MyCards";
 import Pix from "../Pix/Pix";
-import Sent from "../Pix/Sent";
-import { CreateCards } from "../MyCards/styles";
+import Sent from "../Pix/Sent/index";
+import Home from "../Home/home"
+import Extract from "../Extract/extract";
 
 export default function RouterApp () {
   return (
     <>  
     <BrowserRouter>
         <Routes>
-            <Route exact path="/" element={<CreditCard/>} />
+            <Route path="/" element={<Home/> } />
+            <Route  path="/criar" element={<CreditCard/>} />
             <Route  path="/meus-cartoes" element={<MyCards/>} />
-            <Route  exact path="/pagamentos" element={<Pix/>} />
-            <Route exact path="/enviado" element={<Sent/>} />
-            <Route  path="*" element={<CreateCards/>} />
+            <Route  path="/pagamentos" element={<Pix/>} />
+            <Route  path="/enviado" element={<Sent/>} />
+            <Route  path="/extrato" element={<Extract/>} />
+            <Route path="*" element={<Home/> } />
         </Routes>
     </BrowserRouter>
     </>
