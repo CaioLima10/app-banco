ea737c865704d7d09b576736657b1140a28a9962
Caio lima <caiohenrys50@gmail.com>
2023-08-25 02:47:52 -0300

    add account

--- src/components/Pix/Acconut/acconut.jsx
+++ src/components/Pix/Acconut/acconut.jsx
@@ -2,17 +2,11 @@ import { useEffect, useState } from "react";
 import { BsEye } from "react-icons/bs";
 import { BsEyeSlash } from "react-icons/bs";
 import { ContainerOpen } from "./style";
-import { currentMask } from "../mask";
 
 export default function Account() {
     const [open, setOpen] = useState(false);
     const createPix = JSON.parse(localStorage.getItem('Pix')) || [];
 
-    const handleInputChangePrice = (event) => {
-        const price = event.target.value.trim()
-        event.target.value = price
-        return price
-      }
 
     useEffect(() => {
         const localToggleOpen = JSON.parse(localStorage.getItem('toggle'));
@@ -32,16 +26,19 @@ export default function Account() {
         for (const item of createPix) {
             totalValue -= parseFloat(item.number);
         }
-        localStorage.setItem('totalValue', JSON.stringify(totalValue.toFixed(2)));
-        
-        return totalValue.toFixed(2);
+        localStorage.setItem('totalValue', JSON.stringify(totalValue));
+        return totalValue; 
     };
 
     return (
         <ContainerOpen>
-            <div className="number__open__hide">
-                <span onChange={(e) => handleInputChangePrice(currentMask(e))} id="number-open" >R$ {open ? calculateTotalValue() : "******"} </span>
+                <div className="number__open__hide">
+            <div id="title-balance">
+                <span>saldo disponivel</span>
             </div>
+                <span id="number-open"> R$ {open ? calculateTotalValue() : "******"}</span>
+            </div>
+
             <div className="btn__open__hide" onClick={handleToggleOpen}>
                 {open ? <BsEye /> : <BsEyeSlash />}
             </div>
