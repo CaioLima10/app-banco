e5bf321cb36e5a97d0f9552f459e7fa7945eb9c1
Caio lima <caiohenrys50@gmail.com>
2023-08-31 10:39:44 -0300

    fixing the dialog

--- src/components/MyCards/Dialog/index.jsx
+++ src/components/MyCards/Dialog/index.jsx
@@ -1,22 +1,58 @@
-import { Container, ContainerDialog, InfoCircle } from "./styles";
+import { Container, ContainerDialog } from "./styles";
+import { FaRegTimesCircle } from "react-icons/fa";
+import Hiding from "../../../assets/hiding.png";
+import Logo from "../../../assets/BANK-logo.png"
+import { useState } from "react";
 
-export default function Dialog({ message , onDialog}) {
+export default function Dialog({ message, onDialog }) {
+  const [isVisibled, setIsVisibled] = useState(false); 
+  const [isVisibledLogo , setIsVisibledLogo] = useState(true); 
+
+  function handleHiding() {
+    setIsVisibled(false);
+    setIsVisibledLogo(true)
+  }
+
+  function handleMouseEnter() {
+    setIsVisibled(true);
+    setIsVisibledLogo(false)
+  }
+
   return (
-    <Container onClick={() => onDialog(false)}>
-        <ContainerDialog>
-          <InfoCircle/>
-          <h3>{message}</h3>
-            <div className="container-btn">
-              <button 
-              className="confirm"
-              onClick={() => onDialog(true)}
-              >Confirmar</button>
-              <button 
-              className="cancel"
-              onClick={() => onDialog(false)}
-              >Cancelar</button>
-            </div>
-        </ContainerDialog>
-    </Container>
-  )
+    <>
+        <Container onClick={() => onDialog(false)}/>
+      <ContainerDialog>
+        <button 
+          className="cancel"
+          onClick={() => onDialog(false)}>
+          <FaRegTimesCircle />
+
+        </button>
+        { isVisibledLogo && <img className="logo" src={Logo} alt="Logo" /> }
+
+        {isVisibled && (
+          <>
+          <img className="hiding" src={Hiding} alt="Personagem-Escondido" />
+          <div className="container-text">
+            <p>Não faz isso cartão tão bonito!!</p>
+          </div>
+          </>
+        )}
+        <h3>{message}</h3>
+        <div className="bar-color"></div>
+        <div className="container-btn">
+          <button
+            onMouseEnter={handleMouseEnter} 
+            onMouseLeave={handleHiding}
+            className="confirm"
+            onClick={() => onDialog(true)}
+          >
+            Confirmar
+          </button>
+        </div>
+      </ContainerDialog>
+
+
+    </>
+  );
 }
