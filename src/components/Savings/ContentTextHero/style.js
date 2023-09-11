fda5e9b0a97f9046392a273a92423a8844d9cf0a
Caio lima <caiohenrys50@gmail.com>
2023-09-11 13:23:12 -0300

    style spare hero

---
+++ src/components/Savings/ContentTextHero/style.js
@@ -0,0 +1,42 @@
+import { styled } from "styled-components";
+
+export const ContainerPoupaHero = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: auto;
+    width: 100%;
+    position: relative;
+    
+    .content-poupa-hero{
+        width: 300px;
+        height: 380px;
+        position: absolute;
+    }
+
+    h1{
+        color: #D08009;
+    }
+
+    .target{
+        width: 25px;
+    }
+
+    p{
+        margin: 20px 0;
+    }
+
+    h3{
+        font-size: 22px;
+    }
+
+    span{
+        color: #0375B4;
+    }
+
+    h2{
+        font-size: 23px;
+        color: #0375B4;
+    }
+
+`
