6fe71ec2776870befcb0dbc33705814e4a442d94
Caio lima <caiohenrys50@gmail.com>
2023-09-11 13:25:39 -0300

    new route sideBar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -2,7 +2,7 @@ import { FaHome  } from "react-icons/fa"
 import {  Close, Container, ContainerContent, Content } from "./style"
 import SideBarItem from "../SideBarItem/SideBarItem"
 import { AiFillCreditCard } from 'react-icons/ai'
-import { TbSquareRoundedPlusFilled } from "react-icons/tb"
+import { TbSquareRoundedPlusFilled, TbTargetArrow } from "react-icons/tb"
 import { PiNewspaperClippingBold } from "react-icons/pi"
 import { MdPix } from 'react-icons/md'
 import  Logo  from "../../assets/BANK-removebg-preview 1.png"
@@ -26,6 +26,8 @@ export default function SideBar({ active }) {
             <SideBarItem Icon={AiFillCreditCard} Text="Meus Cartões" to="/meus-cartoes"/>
             <SideBarItem Icon={MdPix} Text="Faça seu Pix" to="/pagamentos" />
             <SideBarItem  Icon={PiNewspaperClippingBold}  Text="Extrato" to="/extrato" />
+            <SideBarItem Icon={ TbTargetArrow } Text="POUPA HEROI!" to="/poupanca"></SideBarItem>
+          <label></label>
             <div>
               <img src={Logo} alt="LOGO" />
             </div>
