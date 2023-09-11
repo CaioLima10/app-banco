1ee2c36676f9951c7b660df1e83c25c99db05b42
Caio lima <caiohenrys50@gmail.com>
2023-09-11 13:21:19 -0300

    saving value to localstorage

--- src/components/Pix/Acconut/acconut.jsx
+++ src/components/Pix/Acconut/acconut.jsx
@@ -1,51 +1,69 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BsEye } from "react-icons/bs";
 import { BsEyeSlash } from "react-icons/bs";
 import { ContainerOpen } from "./style";
 
 export default function Account() {
-    const [open, setOpen] = useState(false);
-    const createPix = JSON.parse(localStorage.getItem('Pix')) || [];
+  const [open, setOpen] = useState(false);
+  const createPix = JSON.parse(localStorage.getItem('Pix')) || [];
+  const inputValue = JSON.parse(localStorage.getItem('inputValue')) || 0; 
+  let accountBalance = JSON.parse(localStorage.getItem("accountBalance")) || 30000;
+  const [value, setValue] = useState(0);
 
+  useEffect(() => {
+    const localToggleOpen = JSON.parse(localStorage.getItem('toggle'));
+    if (localToggleOpen !== null) {
+      setOpen(localToggleOpen);
+    }
+  }, []);
 
-    useEffect(() => {
-        const localToggleOpen = JSON.parse(localStorage.getItem('toggle'));
-        if (localToggleOpen !== null) {
-            setOpen(localToggleOpen);
-        }
-    }, []);
+  const handleHideOpen = (event) => {
+    event.preventDefault();
+    setOpen(!open);
+    localStorage.setItem('toggle', JSON.stringify(!open));
+  };
 
-    const handleHideOpen = (event) => {
-        event.preventDefault();
-        setOpen(!open);
-        localStorage.setItem('toggle', JSON.stringify(!open));
-    };
+  const calculateTotalValue = () => {
+    let totalValue = accountBalance;
 
-    const calculateTotalValue = () => {
-        let totalValue = 20000;
-        for (const item of createPix) {
-            totalValue -= parseFloat(item.number);
-        }
-        localStorage.setItem('totalValue', JSON.stringify(totalValue));
-        return totalValue; 
-    };
+    for (const item of createPix) {
+      totalValue -= item.number || 0;
+    }
 
-    return (
-        <ContainerOpen>
-                <div className="number__open__hide">
-            <div id="title-balance">
-                <span>Saldo disponivel</span>
-            </div>
-                <span id="number-open"> R$ {open ? calculateTotalValue() : "******"}</span>
-            </div>
+    if (inputValue === 30000) {
+      totalValue -= inputValue;
+      accountBalance = totalValue;
+      value = accountBalance
+      setValue(value);
+      localStorage.setItem("accountBalance", JSON.stringify(accountBalance));
+    }
 
-            <div className="btn__open__hide" onClick={handleHideOpen}>
-                {open ? <BsEye /> : <BsEyeSlash />}
-            </div>
+    if (accountBalance < inputValue) {
+      alert("Infelizmente você não tem dinheiro suficiente");
+      return totalValue;
+    }
 
-            {createPix.map((item) => (
-                <div key={item.id}></div>
-            ))}
-        </ContainerOpen>
-    );
+    localStorage.setItem('totalValue', JSON.stringify(totalValue));
+
+    return totalValue;
+  };
+
+  return (
+    <ContainerOpen>
+      <div className="number__open__hide">
+        <div id="title-balance">
+          <span>Saldo disponível</span>
+        </div>
+        <span id="number-open"> R$ {open ? calculateTotalValue() : "******"}</span>
+      </div>
+
+      <div className="btn__open__hide" onClick={handleHideOpen}>
+        {open ? <BsEye /> : <BsEyeSlash />}
+      </div>
+
+      {createPix.map((item) => (
+        <div key={item.id}></div>
+      ))}
+    </ContainerOpen>
+  );
 }
