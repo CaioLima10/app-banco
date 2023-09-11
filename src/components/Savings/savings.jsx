f199f8355c546cb34fa250a711a467a206f51125
Caio lima <caiohenrys50@gmail.com>
2023-09-11 13:22:23 -0300

    CDI implementation, price and total

--- src/components/Savings/savings.jsx
+++ src/components/Savings/savings.jsx
@@ -1,22 +1,50 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import Header from "../Header/Header";
 import {  Button, Container, ContainerTitle, ContainerWrapper, Content, Section } from "./style";
 import Hero from "../../assets/noto_man-superhero.png";
 import Resume from "./Resume/index";
-import Acconut from "../Pix/Acconut/acconut"
-
+import ContentTextHero from "./ContentTextHero";
+import StyleColorGlobal from "../styleColorGlobal/index"
+import ImgbottomContainer from "../../assets/capa-fundo-2.png"
 
 export default function Savings() {
-  
-  const data = localStorage.getItem("transaction");
-  const [transactionList, setTransactionList] = useState(data ? JSON.parse(data) : []);
 
-  const [total, setTotal] = useState(0);
-  const [inputValue, setInputValue] = useState("");
-  const [ progress , setProgress ] = useState(0)
-  const [ imc , setImc ] = useState(0)
-  const [ confirmedCdi , setConfirmedCdi ] = useState(0)
+  const initialCdi = JSON.parse(localStorage.getItem("cdi")) || 0;
+  const initialInputValue = JSON.parse(localStorage.getItem("inputValue")) || "";
+  const initialTotal = JSON.parse(localStorage.getItem("total")) || 0;
+  const initialPresentNumber = JSON.parse(localStorage.getItem("presentNumber")) || 0;
+  const initialFullTotal = JSON.parse(localStorage.getItem("fullTotal")) || 0;
+  const initialProgress = JSON.parse(localStorage.getItem("progress")) || 0;
+  const initialAccountBalance = JSON.parse(localStorage.getItem("accountBalance")) || 50000;
+
+  const data = localStorage.getItem("transaction");
+  const initialTransactionList = JSON.parse(localStorage.getItem("transaction")) || [];
+  const [transactionList, setTransactionList] = useState(initialTransactionList);
+  
+  const [cdi, setCdi] = useState(initialCdi);
+  const [inputValue, setInputValue] = useState(initialInputValue);
+  const [total, setTotal] = useState(initialTotal);
+  const [presentNumber, setPresentNumber] = useState(initialPresentNumber);
+  const [fullTotal, setFullTotal] = useState(initialFullTotal);
+  const [ progress , setProgress ] = useState(initialProgress)
+
+  useEffect(() => {
+    const transactionAmounts = transactionList.map((transaction) => parseFloat(transaction.amount)
+    );
+    const calculatedFullTotal = transactionAmounts.reduce((acc, cur) => acc + cur,0);
+    
+    setFullTotal(calculatedFullTotal);
+  }, [transactionList]); 
+
+  useEffect(() => {
+    localStorage.setItem("cdi", JSON.stringify(cdi));
+    localStorage.setItem("inputValue", JSON.stringify(inputValue));
+    localStorage.setItem("total", JSON.stringify(total));
+    localStorage.setItem("presentNumber", JSON.stringify(presentNumber));
+    localStorage.setItem("fullTotal", JSON.stringify(fullTotal));
+    localStorage.setItem("progress" , JSON.stringify(progress))
+  }, [cdi, inputValue, total, presentNumber, fullTotal , progress]);
 
   const handleAddTransaction = () => {
     const amountIncome = transactionList
@@ -26,45 +54,50 @@ export default function Savings() {
     const income = amountIncome.reduce((acc, cur) => acc + cur, 0)
     
     setTotal(income);
+
+    const newTotalUnits = (parseFloat(inputValue) / 1000) * 10.90;
+    const updatedCdi = cdi + newTotalUnits
+    setCdi(updatedCdi);
+
+    if (parseFloat(inputValue) > initialAccountBalance) {
+      alert("Você está sem saldo suficiente para realizar essa transação.");
+      setInputValue("");
+      setCdi(cdi)
+      return;
+    }
+
     if (inputValue.trim() !== "") {
+
+      const newAccountBalance = initialAccountBalance - parseFloat(inputValue);   
+      localStorage.setItem("accountBalance", JSON.stringify(newAccountBalance));
+
       const newTransaction = {
         id: Math.random(),
         expense: true,
-        amount: parseFloat(inputValue),
-        imc: imc
+        amount: parseFloat(inputValue)
       };
 
-
-      const newTotalUnits = (inputValue / 1000) * 10.90;
-
-      const valueCdi = confirmedCdi + newTotalUnits
-      setConfirmedCdi(valueCdi)
-      setImc(newTotalUnits);
-
-      
       const newTotal = total + parseFloat(inputValue);
       setTotal(newTotal);
 
       if (newTotal >= 10000) {
-        setProgress(100); 
+        const numberOfIncrements = Math.floor(newTotal / 10000);
+        setPresentNumber(prev => prev + numberOfIncrements);
+        const remainder = newTotal % 10000;
+        setTotal(remainder);
         setProgress((newTotal / 10000) * 100);
-        alert("parabens vc ganhou caixa surpresa")
-      }else {
-        setProgress((newTotal / 10000) * 100); 
+      } else {
+        setProgress((newTotal / 10000) * 100);
       }
       
-      if (progress === 100) {
-      alert("Parabéns, você chegou aos 10.000!");
-        setProgress(0)
-        }
-  
-        const updatedTransactionList = [...transactionList, newTransaction];
+      const updatedTransactionList = [...transactionList, newTransaction];
+      setTransactionList(updatedTransactionList);
 
-        localStorage.setItem("transaction", JSON.stringify(updatedTransactionList));
-    
-        setTransactionList(updatedTransactionList);
+      localStorage.setItem("transaction", JSON.stringify(updatedTransactionList));
       setInputValue("");
+      return
     }
+
   };
 
   const [isOpen, setIsOpen] = useState(true);
@@ -79,10 +112,10 @@ export default function Savings() {
     <>
       <Header />
       <Section>
-      <Acconut/>
         <ContainerWrapper>
           <ContainerTitle isOpen={isOpen}>
             <Content></Content>
+          <ContentTextHero/>
             <div>
               <img className="hero" src={Hero} alt="Heroi" />
             </div>
@@ -93,16 +126,21 @@ export default function Savings() {
             </Button>
             <form>
               <Resume
+                fullTotal={`R$ ${fullTotal}`}
                 total={`R$ ${total}`} 
                 progress={progress}
-                imc={imc}
+                presentNumber={presentNumber}
+                setPresentNumber={setPresentNumber}
+                cdi={cdi}
                 inputValue={inputValue}
                 setInputValue={setInputValue}
                 handleAddTransaction={handleAddTransaction}
               />
             </form>
+            <img className="img-bottom" src={ImgbottomContainer} alt="capa-do-container" />
           </Container>
         </ContainerWrapper>
+        <StyleColorGlobal/>
       </Section>
     </>
   );
